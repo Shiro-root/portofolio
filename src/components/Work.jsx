728e7be134e5ee7c821cdedcb3b9b411830bca23
Dewangga Tirta Kencana <dewanggatirtakencana25@gmail.com>
2024-10-20 22:41:44 +0700

    update 1.1

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -53,7 +53,7 @@ const Work = () => {
       <div className="container">
         <h2 className="headline-2 mb-8">My portfolio highlights</h2>
 
-        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px))]">
+        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
           {works.map(({ imgSrc, title, tags, projectLink }, key) => (
             <ProjectCard
               key={key}
