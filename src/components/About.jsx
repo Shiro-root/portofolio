19093808ac9fe3bb7d9812f77a0fc127f3ed6c51
Dewangga Tirta Kencana <dewanggatirtakencana25@gmail.com>
2024-11-12 23:33:34 +0700

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,10 +20,9 @@ const About = () => {
       <div className="container">
         <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12 reveal-up">
           <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
-            Welcome! I&apos;m Dewangga, a junior web developer with a knack for
-            crafting visually stunning and highly functional websites. Combining
-            creativity and technical expertise, I transform your vision into a
-            digital masterpiece that excels in both appearance and performance.
+            Welcome! I&apos;m Dewangga, I am a junior developer currently
+            studying at Darmajaya, if you have a problem or have a project in
+            web development, you can contact me and we can discuss costs hahaha
           </p>
 
           <div className="flex flex-wrap items-center gap-4 md:gap-7">
